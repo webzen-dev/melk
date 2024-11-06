0cd4b0ca13e6e15f0156529b5530af3dbc352777
kevin <kevin@kevin>
2024-11-06 20:14:47 +0330

    redsign home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,72 +1,93 @@
-import { useEffect, useState } from "react";
-import HomeBox from "./HomeBox";
-import HomeContainer from "./HomeContainer";
-import PropertyHomeFileList from "./PropertyHomeFileList";
+import { CgProfile } from "react-icons/cg";
+import {
+  GrDocumentStore,
+  GrDocumentTime,
+  GrDocumentUpdate,
+  GrGroup,
+} from "react-icons/gr";
+import { PiOfficeChairThin } from "react-icons/pi";
+import { TbHomeDollar } from "react-icons/tb";
+import { LiaFileContractSolid } from "react-icons/lia";
+import { IoIosSearch, IoIosStarOutline } from "react-icons/io";
+import { IoDocumentAttachOutline } from "react-icons/io5";
+import { TiDocumentDelete } from "react-icons/ti";
+
 const Home = () => {
-  const [score, setScore] = useState<number>(10);
-  const [remainingDays, setRemainingDays] = useState<number>(10);
-  const [remainingMonths, setRemainingMonths] = useState<number>(
-    Math.floor(remainingDays / 30)
-  );
-  const [user, setUser] = useState<number>(5);
-  const maxDays = 300;
-  const calculateRemainingMonths = (days: number) => {
-    return Math.floor(days / 30);
-  };
-  const calculateLineWidth = (days: number) => {
-    const widthPercentage = (days / maxDays) * 100;
-    return widthPercentage > 100 ? 100 : widthPercentage;
-  };
-  useEffect(() => {
-    const months: number = calculateRemainingMonths(remainingDays);
-    setRemainingMonths(months);
-  }, [remainingDays]);
-
   return (
-    <div className="home-page">
-      <HomeContainer />
-      <HomeBox />
-      <PropertyHomeFileList />
-      
-        <div className="account">
-          <div className="score-box">
-            <img src="images/medal.png" alt="score icon" />
-            <div className="score">{score}</div>
-            <span>امتیاز</span>
+    <div className="home-app">
+      <div className="container">
+        <div className="profile">
+          <CgProfile />
+          <div className="user">
+            <span>اسم اکانت </span>
+            <span>کد اکانت : 923843</span>
           </div>
-          <div className="child-box">
-            <div className="info-account">
-              <span>اکانت</span>
-              {/* A warning message when there are less than one month remaining */}
-
-              {/* <span>
-                {remainingDays <= 30
-                  ? "کمتر از یک ماه"
-                  : `${remainingMonths} ماهه`}
-              </span> */}
-
-              {/* If less than 30 days are remaining, show the remaining days, otherwise show the remaining months */}
+        </div>
+        <div className="container-box">
+          <div className="item">
+            دفتر مشتری
+            <GrGroup />
+          </div>
+          <div className="item">
+            دفتر فایل
+            <PiOfficeChairThin />
+          </div>
+          <div className="item">
+            اگهی پلاس
+            <TbHomeDollar />
+          </div>
+          <div className="item">
+            قرارداد های رهن و اجاره
+            <LiaFileContractSolid />
+          </div>
+        </div>
+        <button>رفتن به صفحه ی اگهی ها</button>
+        <div className="input-box">
+          <input type="text" placeholder="ملک مورد نظر خود را جست و جو کنید" />
+          <IoIosSearch />
+        </div>
+        <div className="filing-lits">
+          <div className="title">سبد فایل های پلاس </div>
+          <div className="box">
+            <div className="item">
+              <span>فایلینگ</span>
               <span>
-                {remainingDays <= 30
-                  ? `${remainingDays} روزه`
-                  : `${remainingMonths} ماهه`}
+                <IoDocumentAttachOutline />
               </span>
             </div>
-            <div className="loader">
-              {/* ماهه */}
-              <div
-                className="line"
-                style={{
-                  width: `${calculateLineWidth(remainingDays)}%`,
-                }}
-              ></div>
+            <div className="item">
+              <span>فایلینگ به روز روزرسانی</span>
+              <span>
+                <GrDocumentTime />
+              </span>
             </div>
-            <div className="info-days">
-              <span> {remainingDays} </span>
-              روز باقی مانده - {user} کاربره
+            <div className="item">
+              <span>فایلینگ حذف شده</span>
+              <span>
+                <TiDocumentDelete />
+              </span>
+            </div>
+            <div className="item">
+              <span>فایل داغ</span>
+              <span>
+                <GrDocumentStore />
+              </span>
             </div>
           </div>
         </div>
+        <div className="account">
+          <div className="box">
+            <IoIosStarOutline />
+            <span>۱۰ امتیاز</span>
+          </div>
+
+          <div className="code">
+            <span>کد اکانت </span>
+            <span>#23rfaef</span>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 };
